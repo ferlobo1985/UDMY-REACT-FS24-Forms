03fb73fb17c7b043e36210bfaadbc6033a9e61c4
ferlobo1985 <fernando.lobo.prez@gmail.com>
2023-10-24 03:24:39 -0300

    07-Custom components
    07-Custom components

--- src/components/formThree.jsx
+++ src/components/formThree.jsx
@@ -4,14 +4,36 @@ import * as Yup from 'yup';
 const FormThree = () => {
 
     const formikProps = {
-        initialValues:{firstname:'',color:''},
+        initialValues:{firstname:'',lastname:'',color:'',age:''},
         validationSchema:Yup.object({
-            firstname:Yup.string().required('Sorry, this is required')
+            firstname:Yup.string().required('Sorry, this is required'),
+            lastname:Yup.string().required('Sorry, this is required'),
+            age:Yup.string().required('Sorry, this is required'),
         }),
         onSubmit: values =>{
             console.log(values)
         }
     };
+
+
+    const myCustomComponent = ({
+        field,// {name, value, onchange, onblur}
+        form:{ touched, errors },
+        ...props
+    }) => (
+        <>  
+            <label htmlFor={field.name}>{props.labelname}</label>
+            <input
+                type='text'
+                className='form-control'
+                {...field}
+            />
+            { errors[field.name] && touched[field.name]?
+                <span>{errors[field.name]}</span>
+            :null}
+        </>
+    )
+
 
     return(
         <div className="container">
@@ -25,7 +47,24 @@ const FormThree = () => {
                     { formik.errors.firstname && formik.touched.firstname ? 
                         <span>{formik.errors.firstname}</span>
                     :null}
-             
+                    <hr className="mb-4" />
+
+                    <Field
+                        name="lastname"
+                        placeholder="Last name"
+                        component={myCustomComponent}
+                        labelname="Enter your lastname"
+                    />
+
+                    <hr className="mb-4" />
+
+                    <Field
+                        name="age"
+                        placeholder="Age"
+                        component={myCustomComponent}
+                        labelname="Enter your age"
+                    />
+
                     <hr className="mb-4" />
 
                     <Field
